18bc0e56c0fd12c091d64f338df235988a5fd6dc
felismao <81837957+felismao@users.noreply.github.com>
2023-09-09 02:01:30 +1000

    Corse Enabled

--- weatherStationAPI/weatherStationAPI/Controllers/weatherStationsController.cs
+++ weatherStationAPI/weatherStationAPI/Controllers/weatherStationsController.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using weatherStationAPI.Models;
 
 namespace weatherStationAPI.Controllers
 {
     public class weatherStationsController : ApiController
     {
+        [EnableCors(origins: "http://localhost:3000", headers:"*", methods:"*")]
+        //api/weatherStations to show Json 
         public IEnumerable<weatherStation> Get()
         {
             using (weatherStationDBContext dbContext = new weatherStationDBContext())
