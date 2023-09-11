6d33016193d1d102aebd6b0f06001adfc9d77d6b
felismao <81837957+felismao@users.noreply.github.com>
2023-09-12 00:34:17 +1000

    tables joined

---
+++ weatherStationAPI/weatherStationAPI/Models/JoinClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace weatherStationAPI.Models
+{
+    public class JoinClass
+    {
+        public stationData getstationData {get;set; }
+        public weatherStation getweatherStation { get; set; }
+        
+    }
+}
---
+++ weatherStationAPI/weatherStationAPI/Models/JoinTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace weatherStationAPI.Models
+{
+    public class JoinClass
+    {
+        public stationDataModel getstationData { get; set; }
+        public weatherStationModel getweatherStation { get; set; }
+
+    }
+}
